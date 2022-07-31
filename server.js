af8532b4aa020dfdda21be88a18defd52d9450a5
John Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-07-31 19:26:12 -0400

    Connect to mongodb

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+const PORT = 8000;
+
 let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName;
+    dbName = 'template',
+    collection;
 
 app.use(cors());
 app.use(express.json());
+
+MongoClient.connect(dbConnectionString)
+  .then(client => {
+    console.log('Connected to Database');
+    db = client.db(dbName);
+    collection = db.collection('sampleCollection')
+  })
+
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log('Server is running');
+})
