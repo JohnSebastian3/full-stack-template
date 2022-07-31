9ad6242f7429003f361c0f5a46e3efeba827018a
John Sebastian Guerrero <sebastianbiochem@gmail.com>
2022-07-31 19:13:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName;
+
+app.use(cors());
+app.use(express.json());
